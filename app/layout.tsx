3cb66e97a428d20ca3a2c1aba57cc2d9bc90f2b1
LikeYasay <lichael456@gmail.com>
2025-11-03 16:56:47 +0800

    feature(browser icon): added cit logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,15 +5,22 @@ import { Kumbh_Sans } from "next/font/google";
 
 const kumbh = Kumbh_Sans({
   subsets: ["latin"],
-  weight: ["300","400","500","600","700","800","900"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata = {
   title: "Campus Connect",
   description: "Your all-in-one hub for lost & found and student forums.",
+  icons: {
+    icon: "/CIT_LOGO.png", 
+  },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       {/* Kumbh Sans applied to whole app */}
