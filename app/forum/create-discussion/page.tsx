d5cd29c287a0fc90dcbf005b78a88547ea2ef7d9
LAB24_USER ON WS39 <L24X10W39@LabHCI.net>
2025-12-15 11:18:07 +0800

    fix(discussion): fix not posting

--- app/forum/create-discussion/page.tsx
+++ app/forum/create-discussion/page.tsx
@@ -1,5 +1,10 @@
+import RequireAuth from "@/components/auth/require-auth";
 import { CreateDiscussionForm } from "@/components/forum/create-discussion";
 
 export default function CreateDiscussionPage() {
-  return <CreateDiscussionForm />;
+  return (
+    <RequireAuth>
+      <CreateDiscussionForm />
+    </RequireAuth>
+  )
 }
